0230ca97fc6ebf2a98c4690f148bff27fa3028b8
Temenu Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2026-03-03 23:43:04 +0100

    Finished the hero section for all screens

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,14 +1,71 @@
-import React from 'react'
+import React from "react";
+import { FaMapMarkerAlt, FaChevronDown, FaSearch } from "react-icons/fa";
+import Hero_img from "/src/assets/hero-image.png";
+import instagram from "/src/assets/Instagram.png";
+import Google from "/src/assets/Google.png";
+import Microsoft from "/src/assets/Microsoft.png";
+import Behance from "/src/assets/behance.png";
 
 const Hero = () => {
   return (
-    <div>
-      <h1 className='text-8xl text-center font-bold'>
-        {/* Connecting Talent with Opportunity */}
-      </h1>
-      <p>Explore thousands of job listings in data science, web development, and more.</p>
+    <div className="bg-gray-200 px-9 flex flex-col md:flex-row justify-between items-center">
+      <div className="max-w-[500px]">
+        <div className="hidden bg-blue-100 py-3 rounded-3xl w-[90%] sm:w-[490px]">
+          <span className="bg-[#073aa0] text-white py-2 px-3 rounded-3xl ml-2 mr-3">
+            NEW
+          </span>
+          <span className="font-semibold">
+            Stay connected to get upcoming jobs with #worklynk
+          </span>
+        </div>
+        <h1 className="text-4xl lg:text-5xl font-bold pb-4 pt-9 lg:pt-0">
+          Connecting <span className="text-blue-600">Talent</span> <br />
+          with Opportunity
+        </h1>
+        <p className="text-xl font-semibold pb-8 text-gray-600">
+          Your dream job is waiting for you
+        </p>
+        <div className="rounded-2xl flex flex-col lg:flex-row lg:items-center gap-4 lg:bg-white lg:py-1.5 lg:px-1.5">
+          <input
+            type="text"
+            name=""
+            id=""
+            className="bg-gray-100 py-4 rounded-2xl pl-3 outline-0"
+            placeholder="Job title or keywords"
+          />
+          <div className="relative">
+            <FaMapMarkerAlt className="absolute top-5 left-3 text-gray-300" />
+            <FaChevronDown className="absolute right-6 top-5 text-gray-400" />
+            <input
+              type="text"
+              name=""
+              id=""
+              className="bg-gray-100 py-4 rounded-2xl pl-10 outline-0 w-full"
+              placeholder="City or Zip code"
+            />
+          </div>
+          <button className="bg-blue-200 p-4 rounded-4xl font-bold mb-3 lg:mb-0 lg:hidden">
+            Search
+          </button>
+          <button className="bg-blue-200 p-4 rounded-4xl font-bold mb-5 lg:mb-0 hidden lg:block">
+            <FaSearch/>
+          </button>
+        </div>
+        <div className=" pt-5 lg:pt-12">
+          <p className="font-semibold text-gray-500">We are trusted by:</p>
+          <div className="bg-white flex items-center gap-4 py-4 pl-3 rounded-2xl w-[235px] mt-2 md:mt-5 md:mb-7 lg:mb-0">
+            <img src={Google} alt="" className="w-[40px]"/>
+            <img src={instagram} alt="" className="w-[40px]"/>
+            <img src={Behance} alt="" className="w-[40px]"/>
+            <img src={Microsoft} alt="" className="w-[40px]"/>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src={Hero_img} alt="Team working together" className="mb-7 md:mb-12 mt-10 md:w-[400px] lg:w-[500px]" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,14 +3,14 @@ import logo from "/src/assets/LOGO.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import Hamburger from "hamburger-react";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
-    <nav className="relative top-0 z-50">
-      <div className="flex justify-between items-center px-4 md:px-9 py-5 font-semibold text-[#052d7e]">
-        <div className="w-40 md:w-42 cursor-pointer">
+    <nav className="relative top-0 z-50 bg-gray-200">
+      <div className="flex justify-between items-center px-4 md:px-9 py-5 sm:py-5 font-semibold text-[#052d7e] border-b-blue-100 shadow-xl">
+        <div className="w-32 md:w-42 cursor-pointer">
           <img src={logo} alt="logo" />
         </div>
 
@@ -33,14 +33,14 @@ const Navbar = () => {
         {/* mobile */}
         <div className="md:hidden">
           <button onClick={() => setOpen(!open)} className="text-[#052d7e]">
-            {open ? <IoClose className="text-5xl " /> : <Hamburger />}
+            {open ? <IoClose className="text-3xl" /> : <IoMenu className="text-3xl"/> }
           </button>
         </div>
       </div>
       {/* nav dropdown */}
       <div>
         {open && (
-          <div className="md:hidden bg-white border-t border-gray-100 p-4 space-y-4 shadow-lg absolute w-full">
+          <div className="md:hidden bg-gray-200 border-t border-gray-100 p-4 space-y-4 shadow-lg absolute w-full">
             <div className=" flex flex-col items-center gap-7">
               <Link to="/" onClick={() => setOpen(false)}>Home</Link>
               <Link to="/jobs" className="">
