6a4d174cea4a2887472a1ba98f9833bb92bbb4ff
Temenu Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2026-03-07 19:10:34 +0100

    Implemented the filtering option for the Job cards

--- src/Components/JobContainer.jsx
+++ src/Components/JobContainer.jsx
@@ -7,6 +7,7 @@ const JobContainer = () => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [sortOption, setSortOption] = useState("Newest");
 
   useEffect(() => {
     fetch("https://remotive.com/api/remote-jobs")
@@ -24,11 +25,25 @@ const JobContainer = () => {
   if (loading) return <p className="text-gray-600 mt-4 px-5 font-bold">Loading Jobs....</p>;
   if (error) return <p className="text-gray-600 mt-4 px-5 font-bold">{error}</p>;
 
+  const sortedJobs = [...jobs].sort((a,b) => {
+    if (sortOption == "Newest") {
+      return new Date(b.publication_date) - new Date(a.publication_date);
+    }
+    if (sortOption == "Oldest") {
+      return new Date(a.publication_date) - new Date(b.publication_date);
+    }
+    if (sortOption == "Relevant") {
+      const aSalary = a.salary ? 1 : 0;
+      const bSalary = b.salary ? 1: 0;
+      return bSalary - aSalary
+    }
+  })
+
   return (
     <div className="max-w-[1460px] mx-auto">
-      <JobsHeader count={jobs.length} />
+      <JobsHeader count={sortedJobs.length} sortOption={sortOption} setSortOption={setSortOption} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-5 gap-5 md:mx-9 mb-5 md:mb-10">
-        {jobs.map((job) => (
+        {sortedJobs.map((job) => (
           <JobCard key={job.id} job={job} />
         ))}
       </div>
--- src/Components/JobsHeader.jsx
+++ src/Components/JobsHeader.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const JobsHeader = ({count}) => {
+const JobsHeader = ({count, sortOption, setSortOption}) => {
   return (
     <div className='px-4 md:px-9 py-5 sm:py-5 flex flex-col md:flex-row md:items-center md:justify-between gap-3 mb-6 border-b border-gray-200 pb-6'>
         <div>
@@ -9,10 +9,10 @@ const JobsHeader = ({count}) => {
         </div>
         <div className='flex items-center gap-3'>
             <span className="text-sm text-gray-500">Sort by:</span>
-            <select name="" id="" className='border rounded-lg border-gray-900 text-sm px-3 py-2 bg-gray-100 text-gray-800'>
-                <option value="">Newest</option>
-                <option value="">Oldest</option>
-                <option value="">Most Relevant</option>
+            <select value={sortOption} onChange={(e) => setSortOption(e.target.value) } className='border rounded-lg border-gray-900 text-sm px-3 py-2 bg-gray-100 text-gray-800'>
+                <option value="Newest">Newest</option>
+                <option value="Oldest">Oldest</option>
+                <option value="Relevant">Most Relevant</option>
             </select>
         </div>
     </div>
