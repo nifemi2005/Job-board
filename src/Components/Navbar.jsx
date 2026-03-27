af3b747e3f728f9acb17e52575113b81451ea2d0
Temenu Oluwanifemi <temenuoluwanifemi2005@gmail.com>
2026-03-27 12:59:01 +0100

    Changed the blog link to a 'post a job' link

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -13,7 +13,9 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 bg-gray-200">
       <div className="flex justify-between items-center px-4 md:px-9 py-5 sm:py-5 font-semibold text-[#052d7e] border-b-blue-100 shadow-xl">
         <div className="w-32 md:w-42 cursor-pointer">
-          <img src={logo} alt="logo" />
+          <Link to='/'>
+            <img src={logo} alt="logo" />
+          </Link>
         </div>
 
         {/* deskop */}
@@ -36,7 +38,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="hidden md:flex items-center gap-7">
-          <Link to="/blog">Blog</Link>
+          <Link to="/soon">Post a Job</Link>
           <Link to="/soon" className="btn btn-primary  btn-outline">
             Register Now
           </Link>
@@ -55,7 +57,7 @@ const Navbar = () => {
       {/* nav dropdown */}
       <div>
         {open && (
-          <div className="md:hidden bg-gray-200 border-t border-gray-100 p-4 space-y-4 shadow-lg absolute w-full">
+          <div className="md:hidden bg-gray-200 border-t border-gray-100 p-4 space-y-4 shadow-lg absolute w-full text-[#052d7e] font-semibold">
             <div className=" flex flex-col items-center gap-7">
               <Link to="/" onClick={() => setOpen(false)}>
                 Home
@@ -63,11 +65,14 @@ const Navbar = () => {
               <Link to="/jobs" className="">
                 Jobs
               </Link>
-              <Link to="/" className="">
+              <Link to="/saved" className="">
                 Saved Jobs
               </Link>
-              <Link to="/blog">Blog</Link>
-              <Link to="/register" className="btn">
+              <Link to="/soon">Post a Job</Link>
+              <Link
+                to="/soon"
+                className="btn btn-primary  btn-outline font-semibold"
+              >
                 Register Now
               </Link>
             </div>
